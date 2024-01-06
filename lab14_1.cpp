d13ef961185a6eb50a1579ee1e4f26f84009e9ba
aonnml <radchanon694@gmail.com>
2024-01-06 18:55:12 +0700

    lab14_1.cpp completed

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -3,7 +3,18 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-
+    for(int i = 1 ; i < N;i++){
+		for(int j = i ;j > 0 ; j--){
+			if(d[j] > d[j-1]){
+				T temp = d[j];
+				d[j] = d[j-1];
+				d[j-1] = temp;
+			}
+		}
+		cout << "Pass " << i << ":";
+		for(int i = 0;i < 10;i++) cout << d[i] << " ";	
+		cout << endl;
+	}
 }
 
 int main(){
@@ -17,3 +28,19 @@ int main(){
 	cout << "\nSorted Array:";
 	for(int i = 0; i < 10; i++) cout << a[i] << " ";	
 }
+//template <typename T>
+//void insertionSort(T d[],int N){
+    //int i;
+    //for(i = 1; i < N;i++){
+        //T current = d[i];
+        //T j = i-1;
+        //while(j >= 0 && d[j] < current){
+            //d[j+1] = d[j];
+            //j--;
+        //}
+        //d[j+1] = current;
+        //cout << "Pass " << i << ":";
+        //for(j = 0; j < N;j++) cout << d[j] << " ";
+        //cout << "\n";
+    //}
+//}
